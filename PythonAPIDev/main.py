9fab0e92f148920bd384b68192b7a3ec7843674b
michaeljumah <michaeljumah8@gmail.com>
2024-08-25 22:41:03 +0300

    t

--- PythonAPIDev/main.py
+++ PythonAPIDev/main.py
@@ -11,3 +11,8 @@ async def root():
 @app.get("/getuser")
 async def get_user():
     return {"message ": "hello"}
+
+
+@app.post("/creatposts")
+async def create_post():
+    return {"message": "Succsessfully created"}
