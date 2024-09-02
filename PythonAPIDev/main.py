2e22daf1a84bbbb9ec3ff066a1d17706d34f61f6
michaeljumah <michaeljumah8@gmail.com>
2024-09-02 22:20:35 +0300

    2:16:08

--- PythonAPIDev/main.py
+++ PythonAPIDev/main.py
@@ -68,9 +68,22 @@ def delete_post(id: int):
     
     if index == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail= f"no item with id: {id}")
+                            detail= f"post with id: {id} does not exist")
     
     my_posts.pop(index)
         
     
     return {'message': "post was succesfully deleted"}
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} does not exist")
+        
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
